5a7b8f512dfa532fab20fd9185a1dceb75a44319
Badsha-Nishan <badshanisan14@gmail.com>
2026-02-07 14:40:19 +0600

    completed solution of problem# 2

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-function newPrice(currentPrice, discount) {
-  if (typeof currentPrice === "string" || typeof discount === "string")
+// Problem # 1
+
+// function newPrice(currentPrice, discount) {
+//   if (typeof currentPrice === "string" || typeof discount === "string")
+//     return "Invalid";
+//   const discountedPrice = (currentPrice * discount) / 100;
+//   const newPrice = currentPrice - discountedPrice;
+//   return newPrice.toFixed(3);
+// }
+
+// const price = newPrice(1500, "20");
+// console.log(price);
+
+// Problem # 2
+
+function validOtp(otp) {
+  if (typeof otp !== "string") {
     return "Invalid";
-  const discountedPrice = (currentPrice * discount) / 100;
-  const newPrice = currentPrice - discountedPrice;
-  return newPrice.toFixed(3);
+  } else if (!otp.startsWith("ph-") || otp.length !== 8) {
+    return false;
+  } else return true;
 }
 
-const price = newPrice(1500, "20");
-console.log(price);
+const otp = "ph-23343";
+const result = validOtp(otp);
+console.log(result);
