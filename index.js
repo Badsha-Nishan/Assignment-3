94c8ac8eab0d16aede8e3505823e282d45ff37ad
Badsha-Nishan <badshanisan14@gmail.com>
2026-02-07 14:59:31 +0600

    solution of problem # 3

--- index.js
+++ index.js
@@ -13,14 +13,32 @@
 
 // Problem # 2
 
-function validOtp(otp) {
-  if (typeof otp !== "string") {
+// function validOtp(otp) {
+//   if (typeof otp !== "string") {
+//     return "Invalid";
+//   } else if (!otp.startsWith("ph-") || otp.length !== 8) {
+//     return false;
+//   } else return true;
+// }
+
+// const otp = "ph-23343";
+// const result = validOtp(otp);
+// console.log(result);
+
+// Problem # 3
+
+function finalScore(omr) {
+  if (omr.right + omr.wrong + omr.skip !== 100) {
     return "Invalid";
-  } else if (!otp.startsWith("ph-") || otp.length !== 8) {
-    return false;
-  } else return true;
+  }
+  const pointSubtract = omr.right - omr.wrong / 2;
+  return Math.round(pointSubtract);
 }
 
-const otp = "ph-23343";
-const result = validOtp(otp);
+const obj = {
+  right: 50,
+  wrong: 10,
+  skip: 40,
+};
+const result = finalScore(obj);
 console.log(result);
